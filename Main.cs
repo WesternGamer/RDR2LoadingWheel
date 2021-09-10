1a9f625e3371de892dd4f7ea47c3655e2d0a8324
WesternGamer <kevnguyen474@gmail.com>
2021-09-09 20:26:44 -0400

    Added code for loading wheel to speed up and down.

--- Main.cs
+++ Main.cs
@@ -6,38 +6,19 @@ namespace RDR2LoadingWheel
 {
     public class Main : IPlugin
     {
-        /// <summary>
-        /// Optional, is not required.
-        /// </summary>
-        public void Startup()
-        {
-
-        }
-
-        /// <summary>
-        /// This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
-        /// </summary>
         public void Dispose()
         {
             
         }
 
-        /// <summary>
-        /// This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
-        /// </summary>
         public void Init(object gameInstance)
         {
-            //Use the code below in this method if you are planning on using Harmony to modify existing game methods. Optional, is not required. For more info about how harmony works, go to https://harmony.pardeike.net/
-
             // Starts an instance of Harmony
             Harmony harmony = new Harmony("RDR2LoadingWheel");
             // Patches all patches in the plugin.
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
-        /// <summary>
-        /// This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
-        /// </summary>
         public void Update()
         {
             
---
+++ Utill/Patch_LoadingWheel.cs
@@ -0,0 +1,60 @@
+﻿using HarmonyLib;
+using Sandbox.Graphics;
+using Sandbox.Graphics.GUI;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using VRage.Utils;
+using VRageMath;
+using Color = VRageMath.Color;
+
+namespace RDR2LoadingWheel.Utill
+{
+    [HarmonyPatch(typeof(MyGuiControlRotatingWheel), "Draw")]
+    public class Patch_LoadingWheel : MyGuiControlRotatingWheel
+    {
+        private static float m_globalRotationSpeed;
+
+        public static bool Prefix(MyGuiControlRotatingWheel __instance, float ___m_wheelScale, float __0, Vector2 ___m_textureSize, float ___m_rotatingAngle, float ___m_rotationSpeed)
+        {
+            m_globalRotationSpeed = ___m_rotationSpeed;
+            Vector2 positionAbsolute = __instance.GetPositionAbsolute();
+            Color color = new Color(__0 * new Color(0, 0, 0, 80).ToVector4());
+            DrawWheel(___m_textureSize, positionAbsolute + MyGuiConstants.SHADOW_OFFSET, ___m_wheelScale, color, ___m_rotatingAngle, m_globalRotationSpeed);
+            Color color2 = MyGuiControlBase.ApplyColorMaskModifiers(__instance.ColorMask, __instance.Enabled, __0);
+            DrawWheel(___m_textureSize, positionAbsolute, ___m_wheelScale, color2, ___m_rotatingAngle, m_globalRotationSpeed);
+            void DrawWheel(Vector2 texturesize, Vector2 position, float scale, Color color3, float rotationAngle, float rotationSpeed)
+            {
+                Vector2 normalizedSize = MyGuiManager.GetNormalizedSize(texturesize, scale);
+                //The texture will be enbedded in the plugin, this is just for development.  
+                MyGuiManager.DrawSpriteBatch(@"D:\Games\steamapps\common\SpaceEngineers\Bin64\frame_0_delay-0.02s.png", position, normalizedSize, color3, MyGuiDrawAlignEnum.HORISONTAL_CENTER_AND_VERTICAL_CENTER, false, false, null, rotationAngle, __instance.ManualRotationUpdate ? 0f : rotationSpeed);
+
+                
+            }
+            
+            return false;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            m_globalRotationSpeed++;
+            DelayAction(2000);
+            void DelayAction(int millisecond)
+            {
+                var timer = new DispatcherTimer();
+                timer.Tick += delegate
+
+                {
+                    m_globalRotationSpeed = 0;
+                    timer.Stop();
+                };
+
+                timer.Interval = TimeSpan.FromMilliseconds(millisecond);
+                timer.Start();
+            }
+        }
+
+
+    }
+}
